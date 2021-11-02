05807d9e86038b6697896d37cb2abaa02e646c47
Anthony Obaze <toneyobaze@gmail.com>
2021-11-02 11:04:41 +0000

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View, Image, TextInput } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.profileContainer}>
+        <Image source={require("./assets/profile.png")} style={styles.image} />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Name" style={styles.input} />
+        <TextInput placeholder="Email" style={styles.input} />
+        <TextInput placeholder="Password" style={styles.input} />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30,
+  },
+
+  profileContainer: {
+    flex: 0.3,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  inputContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  image: {
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+  },
+
+  input: {
+    borderWidth: 2,
+    borderColor: "red",
   },
 });
+
+export default App;
